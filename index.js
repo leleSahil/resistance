2a1eeb13b79de9a0d04264aa9f638a09920f6389
Rick Ley <rickwley@gmail.com>
2015-11-15 01:02:11 -0800

    Added some game initialization

--- index.js
+++ index.js
@@ -39,7 +39,8 @@ io.on('connection', function(socket){
     console.log('user added: ' + name);
     clients[name] = socket;
     usernames.push(name);
-    io.emit('new user', name);
+    console.log("All usernames so far: " + usernames);
+    io.emit('new user', usernames);
 
     if (tryStartingGame()) {
       assignTeams();
